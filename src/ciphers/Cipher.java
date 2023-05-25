9028f59f3031964e8972799c215a3d12bef93da3
laura <lmlb500@york.ac.uk>
2023-05-25 19:11:39 +0100

    + Created 'ciphers' package to create ciphers for testing purposes. + Created abstract class Cipher.

---
+++ src/ciphers/Cipher.java
@@ -0,0 +1,15 @@
+package ciphers;
+
+public abstract class Cipher {
+
+    public Cipher() {}
+
+    public String encrypt(String plainText, int key) {
+        return "";
+    }
+
+    public String decrypt(String cipherText, int key) {
+        return "";
+    }
+
+}
